9b0c31b8aa37fded1e013d92ad3c047aafdcf7f5
rawkush <ankushrawat950@gmail.com>
2018-10-24 16:03:48 +0530

    added logical part

--- CODE/CODE.ino
+++ CODE/CODE.ino
@@ -15,7 +15,9 @@ const int triggerR = 7; //Trigger pin of RIGHT Sesnor
 const int echoR = 8; //Echo pin of 1st Sesnor
 
 const int criticalDistance = 100;  // distance that tell there is a wall
-
+int leftDistance;
+int rightDistance;
+int forwardDistance;
 
 
 void setup() {
@@ -88,8 +90,52 @@ digitalWrite(rMotRev,LOW);
 }
 
 
+
+/*###Function to calculate distance###*/ 
+int calculate_distance(int trigger, int echo)
+{
+digitalWrite(trigger, LOW);
+delayMicroseconds(2);
+digitalWrite(trigger, HIGH);
+delayMicroseconds(10);
+digitalWrite(trigger, LOW);
+
+int time_taken = pulseIn(echo, HIGH);
+int dist= time_taken*0.034/2;
+
+//if (dist>50 && dist<60)
+//dist = 50;
+
+return dist;
+}
+
 void loop() {
   // put your main code here, to run repeatedly:
+  leftDistance=calculate_distance(triggerL,echoL);
+  rightDistance=calculate_distance(triggerR,echoR);
+  forwardDistance=calculate_distance(triggerM,echoM);
 
+  Stop();
+  
+  if(leftDistance>criticalDistance){
+    turnLeft();
+  }else{
+
+
+      if(forwardDistance>criticalDistance){
+        moveForward();
+      }else{
+
+        if(rightDistance>criticalDistnce){
+          turnRight();
+        }
+       
+      }
+    
+  }
+  
+  if(forwardDistance<criticalDistance && leftDistance<criticalDistance && rightDistance<criticalDistance){
+    turnCircle();
+  }
 
 }
