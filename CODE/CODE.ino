929d26eea882ae9d7c44044f29392ec4cfd999d8
MauryaD <ankushrawat950@gmail.com>
2018-10-29 23:45:06 +0530

    recreating

--- CODE/CODE.ino
+++ CODE/CODE.ino
@@ -3,54 +3,40 @@ const int rMotFwd=10;
 const int lMotRev=11;
 const int rMotRev=12; 
 
-const int voltL=0;
-const int voltM=1;
-const int voltR=2;
+const int volt=8;
+const int triggerL = 2; //Trigger pin of LEFT Sesnor
+const int echoL = 3; //Echo pin of 1st Sesnor
+const int triggerM = 4; //Trigger pin of MIDDLE Sesnor
+const int echoM = 5;//Echo pin of 2nd Sesnor
+const int triggerR = 6; //Trigger pin of RIGHT Sesnor
+const int echoR = 7; //Echo pin of 1st Sesnor
 
-const int triggerL = 3; //Trigger pin of LEFT Sesnor
-const int echoL = 4; //Echo pin of 1st Sesnor
-const int triggerM = 5; //Trigger pin of MIDDLE Sesnor
-const int echoM = 6;//Echo pin of 2nd Sesnor
-const int triggerR = 7; //Trigger pin of RIGHT Sesnor
-const int echoR = 8; //Echo pin of 1st Sesnor
 
 const int criticalDistance = 100;  // distance that tell there is a wall
 int leftDistance;
 int rightDistance;
 int forwardDistance;
 
-
 void setup() {
+Serial.begin(9600); 
+pinMode(triggerL, OUTPUT); 
+pinMode(echoL, INPUT); 
+pinMode(volt,OUTPUT);
+pinMode(triggerM, OUTPUT); 
+pinMode(echoM, INPUT);
+pinMode(triggerR, OUTPUT); 
+pinMode(echoR, INPUT); 
 
 
-  Serial.begin(9600);
-
-
-  pinMode(voltL,OUTPUT);   
-  pinMode(voltL,OUTPUT);   
-  pinMode(voltrM,OUTPUT);   
-  
-  
-  pinMode(triggerL,OUTPUT);   
-  pinMode(echoL,OUTPUT);   
-  pinMode(triggerM,OUTPUT);   
-  pinMode(echoM,OUTPUT);   
-  pinMode(triggerR,OUTPUT);   
-  pinMode(echoR,OUTPUT); 
 
   pinMode(lMotFwd,OUTPUT); 
   pinMode(rMotFwd,OUTPUT);
   pinMode(lMotRev,OUTPUT);  
   pinMode(rMotRev,OUTPUT); 
 
-  applyVoltageToSensors();
+
 }
 
-void  applyVoltageToSensors(){
-  digitalWrite(voltL,HIGH);
-  digitalWrite(voltR,HIGH);
-  digitalWrite(voltM,HIGH);
-}
 
 void moveForward()
 {
@@ -99,43 +85,22 @@ delayMicroseconds(2);
 digitalWrite(trigger, HIGH);
 delayMicroseconds(10);
 digitalWrite(trigger, LOW);
-
 int time_taken = pulseIn(echo, HIGH);
 int dist= time_taken*0.034/2;
-
-//if (dist>50 && dist<60)
-//dist = 50;
-
 return dist;
 }
 
 void loop() {
+  
   // put your main code here, to run repeatedly:
+  
   leftDistance=calculate_distance(triggerL,echoL);
   rightDistance=calculate_distance(triggerR,echoR);
   forwardDistance=calculate_distance(triggerM,echoM);
-
-  Stop();
-  
-  if(leftDistance>criticalDistance){
-    turnLeft();
-  }else{
-
-
-      if(forwardDistance>criticalDistance){
-        moveForward();
-      }else{
-
-        if(rightDistance>criticalDistnce){
-          turnRight();
-        }
-       
-      }
-    
-  }
-  
-  if(forwardDistance<criticalDistance && leftDistance<criticalDistance && rightDistance<criticalDistance){
-    turnCircle();
-  }
+  Serial.print(leftDistance);
+  Serial.print("\t"); 
+  Serial.print(leftDistance); 
+  Serial.print("\t");
+  Serial.println(leftDistance); 
 
 }
